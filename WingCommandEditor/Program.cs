ed315a64e120f4391b0a5ec1cde8f5d7a03a9ec4
yekoba0 <batuyek442@gmail.com>
2022-09-23 04:47:52 +0300

    Add project files.

---
+++ WingCommandEditor/Program.cs
@@ -0,0 +1,29 @@
+﻿using WingTextEditor.Core;
+
+namespace WingEditor
+{
+    public class WingCommandEditor
+    {
+        static void ExecutingTheWing()
+        {
+            while (true)
+            {
+                string data = "";
+                string tempData = "";
+                while (tempData != "run")
+                {
+                    data += tempData + "\n";
+                    tempData = Console.ReadLine();
+                    if (tempData == "exit")
+                        return;
+                }
+                Console.WriteLine(Wing.Execute(data));
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(ExecutingTheWing));
+            thread.Start();
+        }
+    }
+}
